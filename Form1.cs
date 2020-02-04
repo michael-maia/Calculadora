443852b1cff15894f85da4209d2ae84a11ab5d39
michael-maia <michaelmdasilva@yahoo.com.br>
2020-02-03 21:17:35 -0300

    Problema #17 resolvido

--- Form1.cs
+++ Form1.cs
@@ -199,86 +199,46 @@ namespace Calculadora {
 
         // ------------------------------------------------------------------------------------------
         // Função que altera o que a tecla do teclado vai fazer ao ser pressionada
-        protected override bool ProcessCmdKey(ref Message msg,Keys keyData) {
-            if(keyData == Keys.Divide) {
-                btnDividir.PerformClick();                
-                return true;
-            }
-            // TO-DO : O botão BACKSPACE deve remover o último char da string do txtInputNumeros.Text 
-            else if(keyData == Keys.Back) {
-                var text = txtInputNumeros.Text;
-                txtInputNumeros.Text.Remove(text.Length - 1);
-            }
-
-            else if(keyData == Keys.Subtract) {
-                btnSubtrair.PerformClick();
-                return true;
+        protected override bool ProcessCmdKey(ref Message msg,Keys keyData) {          
+            
+            // Remove o ultimo char ao clicar BACKSPACE e se não tiver mais chars ele colocar um ZERO (#17)
+            if(keyData == Keys.Back) {
+                string text = txtInputNumeros.Text.Remove(txtInputNumeros.Text.Length - 1);
+                txtInputNumeros.Text = text;
+                if(txtInputNumeros.Text == "") {txtInputNumeros.Text = "0";
+                    this.textReplacer = true;
+                }
             }
 
-            else if(keyData == Keys.Enter) {
-                btnIgual.PerformClick();
-                return true;
-            }
+            else if(keyData == Keys.Divide) { btnDividir.PerformClick(); return true; }
 
-            else if(keyData == Keys.Add) {
-                btnSomar.PerformClick();
-                return true;
-            }
+            else if(keyData == Keys.Subtract) { btnSubtrair.PerformClick(); return true; }
 
-            else if(keyData == Keys.Multiply) {
-                btnMultiplicar.PerformClick();
-                return true;
-            }
+            else if(keyData == Keys.Enter) { btnIgual.PerformClick(); return true; }
 
-            else if(keyData == Keys.D0 || keyData == Keys.NumPad0) {
-                btnZero.PerformClick();
-                return true;
-            }
+            else if(keyData == Keys.Add) { btnSomar.PerformClick(); return true; }
 
-            else if(keyData == Keys.D1 || keyData == Keys.NumPad1) {
-                btnUm.PerformClick();
-                return true;
-            }
+            else if(keyData == Keys.Multiply) { btnMultiplicar.PerformClick(); return true; }
 
-            else if(keyData == Keys.D2 || keyData == Keys.NumPad2) {
-                btnDois.PerformClick();
-                return true;
-            }
+            else if(keyData == Keys.D0 || keyData == Keys.NumPad0) { btnZero.PerformClick(); return true; }
 
-            else if(keyData == Keys.D3 || keyData == Keys.NumPad3) {
-                btnTres.PerformClick();
-                return true;
-            }
+            else if(keyData == Keys.D1 || keyData == Keys.NumPad1) { btnUm.PerformClick(); return true; }
 
-            else if(keyData == Keys.D4 || keyData == Keys.NumPad4) {
-                btnQuatro.PerformClick();
-                return true;
-            }
+            else if(keyData == Keys.D2 || keyData == Keys.NumPad2) { btnDois.PerformClick(); return true; }
 
-            else if(keyData == Keys.D5 || keyData == Keys.NumPad5) {
-                btnCinco.PerformClick();
-                return true;
-            }
+            else if(keyData == Keys.D3 || keyData == Keys.NumPad3) { btnTres.PerformClick(); return true; }
 
-            else if(keyData == Keys.D6 || keyData == Keys.NumPad6) {
-                btnSeis.PerformClick();
-                return true;
-            }
+            else if(keyData == Keys.D4 || keyData == Keys.NumPad4) { btnQuatro.PerformClick(); return true; }
 
-            else if(keyData == Keys.D7 || keyData == Keys.NumPad7) {
-                btnSete.PerformClick();
-                return true;
-            }
+            else if(keyData == Keys.D5 || keyData == Keys.NumPad5) { btnCinco.PerformClick(); return true; }
 
-            else if(keyData == Keys.D8 || keyData == Keys.NumPad8) {
-                btnOito.PerformClick();
-                return true;
-            }
+            else if(keyData == Keys.D6 || keyData == Keys.NumPad6) { btnSeis.PerformClick(); return true; }
 
-            else if(keyData == Keys.D9 || keyData == Keys.NumPad9) {
-                btnNove.PerformClick();
-                return true;
-            }
+            else if(keyData == Keys.D7 || keyData == Keys.NumPad7) { btnSete.PerformClick(); return true; }
+
+            else if(keyData == Keys.D8 || keyData == Keys.NumPad8) { btnOito.PerformClick(); return true; }
+
+            else if(keyData == Keys.D9 || keyData == Keys.NumPad9) { btnNove.PerformClick(); return true; }
 
             return base.ProcessCmdKey(ref msg,keyData);
         }        
